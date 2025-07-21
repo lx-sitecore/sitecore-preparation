c6694fcca733139df264bef54f3089431d3e2a32
Kiryl Dryha <kiryl.dryha@sap.com>
2025-07-21 18:20:53 +0200

    add header component

---
+++ headapps/nextjs-starter/src/components/HeaderComponent.tsx
@@ -0,0 +1,24 @@
+import { JSX } from 'react';
+import {
+  Text,
+  RichText, Field,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface HeaderComponentProps {
+  fields: {
+    title: Field<string>;
+    body: Field<string>;
+  };
+}
+
+export const Default = (props: HeaderComponentProps): JSX.Element => {
+  return (
+    <div>
+      My most beautiful header component
+      <div className="component-content">
+        <Text field={props.fields.title}></Text>
+          <RichText field={props.fields.body}></RichText>
+      </div>
+    </div>
+  );
+};
